b876547aaee4efee422f968937fcd56add7c9535
psydf6 <psydf6@nottingham.ac.uk>
2024-04-13 17:38:32 +0200

    pls

--- JS/script.js
+++ JS/script.js
@@ -6,6 +6,17 @@ import {createClient} from
 const supabaseUrl = 'https://jizmrczvocrrklokuhua.supabase.co';
 const supabase = createClient(supabaseUrl, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imppem1yY3p2b2Nycmtsb2t1aHVhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTI4NzYyOTYsImV4cCI6MjAyODQ1MjI5Nn0.1IDohWlAcuLG1KXTtAJNFmmFGivtdtbHBl2iL3cYq90');
 
+// Fetch data from the table
+async function fetchData2() {
+   const { data, error } = await supabase
+   .from('People')
+   .select('Name');
+   console.log('Fetched data:', data);
+ }
+ // Call the fetchData function to retrieve data
+ fetchData2();
+
+
 async function fetchData() {
    const { data, error } = await supabase
       .from('People')
